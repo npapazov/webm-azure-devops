549cd753ed7ff5e032afd0ad7b8d06fffbc88985
bpas <bpas@eur.ad.sag>
2020-06-01 09:01:53 +0300

    do not apply conf and reapplly lice on restart

--- charts/universal-messaging/files/init.sh
+++ charts/universal-messaging/files/init.sh
@@ -1,6 +1,10 @@
 import_configuration(){
-  until	/opt/softwareag/UniversalMessaging/tools/runner/runUMTool.sh ImportRealmXML -rname=nsp://localhost:9000 -filename=/tmp/config.xml -importall=true; do sleep 10; done
-
+	if [ ! -f /opt/softwareag/common/conf/.persistent/initialized ] && [ -f /tmp/config.xml ]
+	then
+		echo " importing configuration"
+  		until	/opt/softwareag/UniversalMessaging/tools/runner/runUMTool.sh ImportRealmXML -rname=nsp://localhost:9000 -filename=/tmp/config.xml -importall=true; do sleep 10; done
+		touch /opt/softwareag/common/conf/.persistent/initialized
+	fi
 }
 start_UM(){
 	/bin/sh -c umstart.sh&	
@@ -17,8 +21,23 @@ copy_external_files(){
                 cat /tmp/externalFilesLocations | while read line || [ -n "$line" ]; do echo $line; sh -c "cp /tmp/$line|| true" ; done
         fi
 }
+create_persistent_folder(){
+	if [ ! -d /opt/softwareag/common/conf/.persistent ]
+	then
+		mkdir -p /opt/softwareag/common/conf/.persistent
+	fi
+}
+link_license(){
+	if [ -f /opt/softwareag/common/conf/.persistent/licence.xml ]
+        then
+                rm -f /opt/softwareag/UniversalMessaging/server/umserver/licence/licence.xml
+                ln -s /opt/softwareag/common/conf/.persistent/licence.xml /opt/softwareag/UniversalMessaging/server/umserver/licence/licence.xml
+        fi
+}
+create_persistent_folder
 copy_files
 copy_external_files
+link_license
 start_UM
 PID_TO_WAIT=$!
 # put custom code that should be executed after the UM has been started here
